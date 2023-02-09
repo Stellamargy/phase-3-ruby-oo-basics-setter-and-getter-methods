b88739f058c3e696322e3644dff56129c5d00b53
Stellamargy <stella.oloo@student.moringaschool.com>
2023-02-09 22:32:35 +0300

    completed lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
+    #setter
+    def name=(name)
+        @name=name
+      
+    end
+    #getter
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed=breed
+    end
+
+    def breed
+        @breed
+    end
 
 end
+fido = Dog.new
+snoopy = Dog.new
+fido.name="Fido"
+snoopy.breed="Beagle"
+ puts fido.name
+ puts snoopy.breed
+
